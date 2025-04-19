affc9bf8a54dd590c365ba545bad0cfb928d50e4
expconnor <connor.risse@web.de>
2025-04-19 14:40:10 +0100

    fixed jsx entry point

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,11 +10,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
             sourceType: "module",
           },
         },
