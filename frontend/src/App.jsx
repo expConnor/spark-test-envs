8a40e26a419bb6a33e48c754e63bf4c7bf62d2ae
expconnor <connor.risse@web.de>
2025-04-19 14:27:52 +0100

    setup frontend placeholder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css"; // Import the CSS file
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="decorative-circle circle-1"></div>
+      <div className="decorative-circle circle-2"></div>
+      <div className="content">
+        <h1 className="title">React Placeholder</h1>
+        <div className="divider"></div>
+        <p className="subtitle">Minimal & Modern</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            sourceType: "module",
+          },
+        },
+      },
+      {
+        // New rule for CSS
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  mode: "development",
+};
