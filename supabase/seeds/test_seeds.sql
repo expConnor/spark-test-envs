f924c9cc29708ec90c81f0e7703d833eddab5ecf
expconnor <connor.risse@web.de>
2025-04-21 22:15:09 +0100

    removed test table

--- supabase/seeds/test_seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO test_table (name, description) VALUES
-    ('Test Item 1', 'This is the first test item'),
-    ('Test Item 2', 'This is the second test item'),
-    ('Test Item 3', 'This is the third test item'),
-    ('Test Item 4', 'This is the fourth test item'),
-    ('Test Item 5', 'This is the fifth test item'); 
